0a292aeac44dc9417703219b48275c1c798727ea
Steven Lim <choonwoo49@gmai.com>
2025-02-19 09:45:33 +0900

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ClimbingSystem/UHT/ClimbingSystemCharacter.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ClimbingSystemCharacter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CLIMBINGSYSTEM_ClimbingSystemCharacter_generated_h
+#error "ClimbingSystemCharacter.generated.h already included, missing '#pragma once' in ClimbingSystemCharacter.h"
+#endif
+#define CLIMBINGSYSTEM_ClimbingSystemCharacter_generated_h
+
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_SPARSE_DATA
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_ACCESSORS
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAClimbingSystemCharacter(); \
+	friend struct Z_Construct_UClass_AClimbingSystemCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AClimbingSystemCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ClimbingSystem"), NO_API) \
+	DECLARE_SERIALIZER(AClimbingSystemCharacter)
+
+
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AClimbingSystemCharacter(AClimbingSystemCharacter&&); \
+	NO_API AClimbingSystemCharacter(const AClimbingSystemCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AClimbingSystemCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AClimbingSystemCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AClimbingSystemCharacter) \
+	NO_API virtual ~AClimbingSystemCharacter();
+
+
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_18_PROLOG
+#define FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_SPARSE_DATA \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_ACCESSORS \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_INCLASS_NO_PURE_DECLS \
+	FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CLIMBINGSYSTEM_API UClass* StaticClass<class AClimbingSystemCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ClimbingSystem_Source_ClimbingSystem_ClimbingSystemCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
